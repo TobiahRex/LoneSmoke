188e23f149fc526aa401b393fd1b9b10b821fd81
TobiahRex <tobiah.rex@gmail.com>
2017-06-29 21:35:32 +0700

    building new loneSmoke lambda - "handleSESstatus".

---
+++ serverless/processSESstatus.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-console */
+
+require('dotenv').load({ silent: true });
+
+export default (type) =>
+new Promise((resolve, reject) => {
+  const typeKeys = Object.keys(type);
+  
+  // if type === "Bounce"
+  if (typeKeys.includes('')) {
+
+  }
+});
