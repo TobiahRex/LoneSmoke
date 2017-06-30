26750d361dfba03387b00bba7dd862b11ec89495
TobiahRex <tobiah.rex@gmail.com>
2017-06-30 22:32:56 +0700

    reconfiguring serverless.yml.

--- serverless/handler.js
+++ serverless/handler.js
@@ -4,7 +4,7 @@ if (!global._babelPolyfill) require('babel-polyfill');
 import handleSesStatus from './handleSesStatus';
 import { startDB } from './db/mongo/connection';
 
-module.exports.discount = (event, context, cb) => {
+module.exports.sesStatusHandler = (event, context, cb) => {
   console.log('\nEVENT: ', JSON.stringify(event, null, 2));
   // 3a. Send user a 200 status code and an email that says - "Thank you for signing up with LoneSmoke.  Show this email when you pay and receive 10% off your meal.".
   startDB()
