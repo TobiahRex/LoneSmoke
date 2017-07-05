2137b3c62b1d11f7758a44505d6151e4b35ca5d4
TobiahRex <tobiah.rex@gmail.com>
2017-07-06 03:37:40 +0700

    updated marketHero.sj

--- serverless/db/mongo/models/marketHero.js
+++ serverless/db/mongo/models/marketHero.js
@@ -64,7 +64,7 @@ export default (db) => {
       console.log(`
         Market Hero API Success:
         Created/Updated "${userEmail}".
-        Response: ${JSON.stringify(res)}
+        Response: ${res.statusText}
       `);
     })
     .catch((error) => {
